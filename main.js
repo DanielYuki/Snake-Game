b7edd605855a0fbf3900e7fa162ade43e8b5922f
DYuki <unblivion@gmail.com>
2019-03-06 20:14:28 -0300

    First Commit / Files Set Up

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+// Register serviceworker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('service-worker.js')
+    .then(function() { 
+        console.log('Service Worker Registered'); 
+    }, function(error){
+        console.error(error);
+    });
+}
