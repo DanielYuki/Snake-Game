eb3ef6c0f098acb71827ff47e117aaee924d2bfb
DYuki <unblivion@gmail.com>
2019-03-07 20:23:38 -0300

    Game Logic - 70% / Improvements Coming Soon

--- main.js
+++ main.js
@@ -2,38 +2,94 @@
 const canvas = document.querySelector('#game');
 const ctx = canvas.getContext("2d");
 
-const squareSize = (canvas.clientWidth / 16);
+let canvasSize = canvas.width;
+const squareSize = (canvasSize / 16);
 
-let snake = [];
-snake[0] = {
-    x: 9 * squareSize,
-    y: 10 * squareSize
-}
-
-let food = {
-    x: Math.floor((Math.random() * 16) * squareSize),
-    y: Math.floor((Math.random() * 16) * squareSize)
-}
-
+let face;
 let score = 0;
 
+// Snake is an array
+let snake = [];
+snake[0] = {
+    x: 8 * squareSize,
+    y: 8 * squareSize
+}
+
+// food is an object
+let food = {
+    x: Math.floor((Math.random() * 16)) * squareSize,
+    y: Math.floor((Math.random() * 16)) * squareSize
+}
+
+
+document.addEventListener('keydown', faceection);
+
+function faceection(event) {
+    let key = event.keyCode;
+    if (key == 37 && face != 'RIGHT') {
+        face = 'LEFT'
+    } else if (key == 38 && face != "DOWN") {
+        face = 'UP'
+    }
+    else if (key == 39 && face != "LEFT") {
+        face = 'RIGHT'
+    }
+    else if (key == 40 && face != "UP") {
+        face = 'DOWN'
+    }
+}
+
 function draw() {
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, canvasSize, canvasSize)
+
     for (let i = 0; i < snake.length; i++) {
         ctx.fillStyle = (i == 0) ? "black" : "gray";
         ctx.fillRect(snake[i].x, snake[i].y, squareSize, squareSize);
-        console.log('oi')
+
         ctx.strokeStyle = 'red';
         ctx.strokeRect(snake[i].x, snake[i].y, squareSize, squareSize);
 
     }
+    ctx.fillStyle = 'green'
     ctx.fillRect(food.x, food.y, squareSize, squareSize)
+
+
+    let snakeHeadX = snake[0].x;
+    let snakeHeadY = snake[0].y;
+
+    if (face == 'LEFT') { snakeHeadX -= squareSize }
+    if (face == 'UP') { snakeHeadY -= squareSize }
+    if (face == 'RIGHT') { snakeHeadX += squareSize }
+    if (face == 'DOWN') { snakeHeadY += squareSize }
+
+    if (snakeHeadX == food.x && snakeHeadY == food.y) {
+        score++
+        console.log(score)
+        food = {
+            x: Math.floor((Math.random() * 16)) * squareSize,
+            y: Math.floor((Math.random() * 16)) * squareSize
+        }
+    } else {
+        snake.pop();
+    }
+
+    if (snakeHeadX < 0 || snakeHeadX >= canvasSize ||
+        snakeHeadY < 0 || snakeHeadY >= canvasSize) {
+        clearInterval(gameOn)
+        console.log('over')
+    }
+
+    let newSnakeHeadPosition = {
+        x: snakeHeadX,
+        y: snakeHeadY
+    }
+
+    snake.unshift(newSnakeHeadPosition)
 }
 
-// let gameOn = setInterval(draw, 1000);
 
-console.log(squareSize)
-console.log(food.y)
-console.log(food.x)
+let gameOn = setInterval(draw, 100);
 
 
 // Register serviceworker
